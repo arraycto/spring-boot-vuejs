16e9abb58b7c2ec75262ee5eaa66d063623eeb63
吴仙杰 <wuxianjiezh@gmail.com>
2020-04-01 14:44:51 +0800

    代码重构

--- backend/src/main/java/net/wuxianjie/springbootvuejs/exception/IllegalArgumentException.java
+++ backend/src/main/java/net/wuxianjie/springbootvuejs/exception/IllegalArgumentException.java
@@ -1,6 +1,5 @@
 package net.wuxianjie.springbootvuejs.exception;
 
-import lombok.Getter;
 import net.wuxianjie.springbootvuejs.constants.RestCodeEnum;
 
 /**
@@ -8,18 +7,13 @@ import net.wuxianjie.springbootvuejs.constants.RestCodeEnum;
  *
  * @author 吴仙杰
  */
-public class IllegalArgumentException extends RuntimeException {
-
-  @Getter
-  private RestCodeEnum code;
+public class IllegalArgumentException extends BaseException {
 
   public IllegalArgumentException(String message, RestCodeEnum code) {
-    super(message);
-    this.code = code;
+    super(message, code);
   }
 
   public IllegalArgumentException(String message, Throwable cause, RestCodeEnum code) {
-    super(message, cause);
-    this.code = code;
+    super(message, cause, code);
   }
 }
